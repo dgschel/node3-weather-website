188408b01e4676799cdac6cb0ae27aec65bc91a3
David Göschel <goesc_@hotmail.de>
2020-06-17 10:03:13 +0200

    Remove unnecesary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side js file loaded')
-
 const weatherForm = document.querySelector('form');
 const addressInput = document.querySelector('input');
 const error = document.querySelector('#error');
